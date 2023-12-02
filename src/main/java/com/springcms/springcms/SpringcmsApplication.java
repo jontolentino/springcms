87a39eb9afc91cba523dd573e266dd540e539bd9
qzqz <jontolentino123@gmail.com>
2023-12-02 18:51:16 +0800

    Initial commit, start of SpringCMS :)

---
+++ src/main/java/com/springcms/springcms/SpringcmsApplication.java
@@ -0,0 +1,13 @@
+package com.springcms.springcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcmsApplication.class, args);
+	}
+
+}
