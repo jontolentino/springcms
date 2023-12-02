451cbbfca1820a18ed10b2afeb373db051523fb8
qzqz <jontolentino123@gmail.com>
2023-12-02 19:27:24 +0800

    -Add packages for controller, model. -Add -temp for testing purposes.

---
+++ src/main/java/com/springcms/springcms/controller/TempController.java
@@ -0,0 +1,8 @@
+package com.springcms.springcms.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TempController {
+
+}
---
+++ src/main/java/com/springcms/springcms/model/TempModel.java
@@ -0,0 +1,11 @@
+package com.springcms.springcms.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TempModel {
+    private String firstName;
+    private String lastName;
+}
