6802feae294927fabda1c54dbf3baa9aa8705ca0
qzqz <jontolentino123@gmail.com>
2023-12-02 20:01:29 +0800

    Get request and sql connection
    - Get request on temp controller

--- src/main/java/com/springcms/springcms/controller/TempController.java
+++ src/main/java/com/springcms/springcms/controller/TempController.java
@@ -1,8 +1,13 @@
 package com.springcms.springcms.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TempController {
+    @GetMapping("/temp")
+    public String getExample() {
+        return "Hello, this is the response for GET request";
+    }
 
 }
